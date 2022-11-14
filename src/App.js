d3188dde5ea26c1ce34b4c43bf798b9385e78742
Stephanie <stephanie@Stephanies-MacBook-Air-4.local>
2022-11-14 12:33:12 -0500

    changing state to array

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ src/App.js
 This is the top-level component of the app.
 It contains the top-level state.
 ==================================================*/
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 //importing axios to read the data from the heroku api page
-import axios from 'axios'; 
+import axios from 'axios';
 
 // Import other components
 import Home from './components/Home';
@@ -18,85 +18,106 @@ import Debits from './components/Debits';
 //globally calling time once instead of calling it twice for both debit and credit
 var currentDate = new Date();
 var Month = currentDate.getUTCMonth() + 1;
-var Day = currentDate.getUTCDate();
+var Day = currentDate.getUTCDay();
 var Year = currentDate.getUTCFullYear();
 var Time = Year + '-' + Month + '-' + Day;
 
 class App extends Component {
   constructor() {  // Create and initialize state
-    super(); 
-    this.state = {
-      accountBalance: 1234567.89,
-      debitList: [],
-      currentUser: {
-        userName: 'Joe Smith',
-        memberSince: '11/22/99',
-      },
-      //creating two Arrays to be used to store data for debit and credit
-      creditsArray: [],
-      debitsArray: [],
-      
-    }
+    super();
+    this.state = {} //making each user have their own array so they have their own balance
+    //   1,//first num will be index of current user
+    //   {
+    //     userName: 'Joe Smith',
+    //     memberSince: '11/22/99',
+    //     accountBalance: 1234567.89,
+    //     creditsArray: [],
+    //     debitsArray: []
+    //   }
+    // ]
   }
 
   //checks heroku for both debit and credit (Populates the pages with the preexisting items purchased)
   async componentDidMount() {
-     // setting the API link to a variable
+    // setting the API link to a variable
     let creditAPI = 'https://moj-api.herokuapp.com/credits'
     let debitAPI = 'https://moj-api.herokuapp.com/debits';
-    
+
     //setting a variable to the API response to the link
     let credit = await axios.get(creditAPI);
     let debit = await axios.get(debitAPI);
-    
+
     //sets the array of items on the linked page to the data
-    this.setState({creditsArray: credit.data});
-    this.setState({debitsArray: debit.data});
-    
+    this.setState({
+      currentUser:0,//first num will be index of current user
+      all_Users: [
+        {userName: 'Joe Smith',
+        memberSince: '11/22/99',
+        accountBalance: 1234567.89,
+        creditsArray: credit.data,
+        debitsArray: debit.data}
+      ]});
   }
 
   // Update state's currentUser (userName) after "Log In" button is clicked
-  mockLogIn = (logInInfo) => {  
-    const newUser = {...this.state.currentUser}
+  mockLogIn = (logInInfo) => {
+    const newUser = { ...this.state.currentUser }
     newUser.userName = logInInfo.userName
-    this.setState({currentUser: newUser})
-  }
+    
+    var currentDate = new Date();
+    var Month = currentDate.getUTCMonth() + 1;
+    var Day = currentDate.getUTCDay();
+    var Year = currentDate.getUTCFullYear();
+    var Time = Year + '-' + Month + '-' + Day;
 
+    let shallowState = [...this.state]
+    shallowState.push({
+      userName: newUser.userName,
+      memberSince: Time,
+      accountBalance: 0,
+      creditsArray: [],
+      debitsArray: []
+    })
+    shallowState[0] = shallowState.length()-1
+    this.setState(shallowState)
+  }
+  //HERE ...prevState.creditsArray, { amount: event.target.amount.value, description: event.target.description.value, date: Time }
   // helper function alongside addDebit in Debits.js to add Debits
   addDebits = (event) => {
 
     // if empty there is nothing to add so return aka dont do anything
-    if (event.target.description.value === "" || event.target.amount.value === ""){
+    if (event.target.description.value === "" || event.target.amount.value === "") {
       return;
     }
-    else{
-      this.setState((prevState) => ({ 
+    else {
+      // let shallowState = []
+      this.setState((prevState) => ({
         // states the balance to the nearest 2 digits as requested
-        accountBalance: (this.state.accountBalance - event.target.amount.value).toFixed(2), 
+        accountBalance: (this.state.accountBalance - event.target.amount.value).toFixed(2),
         // triple dot copies the contents of the debitArray
-        debitsArray: [...prevState.debitsArray, {amount: event.target.amount.value, description: event.target.description.value, date: Time}],
-    }))
-   }
+        debitsArray: [...prevState.debitsArray, { amount: event.target.amount.value, description: event.target.description.value, date: Time }],
+      }))
+    }
   }
 
   addCredits = (event) => {
     // if empty, don't do anything and return
     // if(this.state.creditsArray.indexOf(event.target.description.value) !== -1)
     //   return;
-    if(event.target.description.value === "" || event.target.amount.value === ""){
+    if (event.target.description.value === "" || event.target.amount.value === "") {
       return;
     }
     this.setState((prevState) => ({
       // add to account balance as credit is positive
       accountBalance: (Number(this.state.accountBalance) + Number(event.target.amount.value)).toFixed(2),
       // show in the credits array along with the previous credit records
-      creditsArray: [...prevState.creditsArray, {amount : event.target.amount.value, description: event.target.description.value, date: Time}],
+      creditsArray: [...prevState.creditsArray, { amount: event.target.amount.value, description: event.target.description.value, date: Time }],
     }))
   }
 
 
   // Create Routes and React elements to be rendered using React components
-  render() {  
+  render() {
     // Create React elements and pass input props to components
     const HomeComponent = () => (<Home accountBalance={this.state.accountBalance} />);
     const UserProfileComponent = () => (
@@ -105,20 +126,20 @@ class App extends Component {
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
 
     // Debit component will state the account balance on the page alongside being able to add debit and display the array
-    const DebitsComponent = () => (<Debits accountBalance = {this.state.accountBalance} debitsArray = {this.state.debitsArray} addDebits = {this.addDebits} />); 
+    const DebitsComponent = () => (<Debits accountBalance={this.state.accountBalance} debitsArray={this.state.debitsArray} addDebits={this.addDebits} />);
 
     // Credit component will state the account balance on the page alongside being able to add ceredit and display the array
-    const CreditsComponent = () => (<Credits accountBalance = {this.state.accountBalance} creditsArray = {this.state.creditsArray} addCredits = {this.addCredits} />);
+    const CreditsComponent = () => (<Credits accountBalance={this.state.accountBalance} creditsArray={this.state.creditsArray} addCredits={this.addCredits} />);
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
-    return(
+    return (
       <Router basename="/WebDevelopmentAssignment4">
         <div>
-          <Route exact path="/" render={HomeComponent}/>
-          <Route exact path="/userProfile" render={UserProfileComponent}/>
-          <Route exact path="/login" render={LogInComponent}/>
-          <Route exact path="/credits" render={CreditsComponent}/>
-          <Route exact path="/debits" render={DebitsComponent}/>
+          <Route exact path="/" render={HomeComponent} />
+          <Route exact path="/userProfile" render={UserProfileComponent} />
+          <Route exact path="/login" render={LogInComponent} />
+          <Route exact path="/credits" render={CreditsComponent} />
+          <Route exact path="/debits" render={DebitsComponent} />
         </div>
       </Router>
     );
